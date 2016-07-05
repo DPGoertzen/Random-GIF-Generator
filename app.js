3d2b7b8e2f988ccb699547e320624f0d022f29bb
DPGoertzen <dpgoertzen@gmail.com>
2016-07-04 19:10:04 -0500

    Pro mode unlocked, page brutalized (as in the visual style of Brutalism).

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+// Dear Connor/Ryan/Joel: This is objectively stupidly written code.
+// The variable and function names are often non-descript, and there
+// are next to no comments. However, a weekend without touching code
+// did my brain a tremendous amount of good.
+
 $(function(){
   var randomGIF = "";
-  var tag = "";
+  var gifTag = "";
+  var randomSticker = "";
+  var stickerTag = "";
+  var search = "";
+  var searchTag = "";
+  var i = 0;
+  var thing = "";
+
+// handles random gif form submission.
   $(".makeGIF").on("submit", function(event){
     event.preventDefault();
-    tag = $(this.tag).val();
-
-    console.log(tag);
-    if(tag != ""){
-      randomGIF = $.ajax("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + tag);
+    gifTag = $(this.gifTag).val();
+    if(gifTag != ""){
+      randomGIF = $.ajax("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + gifTag);
     } else{
       randomGIF = $.ajax("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC");
     }
@@ -18,13 +29,62 @@ $(function(){
     }).catch(function(){
 
       $(".container").append("<p>nothing appeared</p>")
-
     });
-
-
-
   });
 
+// Handles sticker form submission.
+  $(".makeSticker").on("submit", function(event){
+    event.preventDefault();
+    stickerTag = $(this.stickerTag).val();
+    if(stickerTag != ""){
+      randomSticker = $.ajax("http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=" + stickerTag);
+    } else{
+      randomSticker = $.ajax("http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC");
+    }
+    randomSticker.then(function(data){
+      var sticker = data.data.image_url;
+      $(".container").empty();
+      $(".container").append('<img src="' + sticker + '" />')
+    }).catch(function(){
+      $(".container").append("<p>nothing appeared</p>")
+    });
+  });
+
+// The following is some hot garbage code made of fairy whispers
+// and recycled duct tape. DO NOT TOUCH.
+  $(".makeSearch").on("submit", function(event){
+    event.preventDefault();
+    searchTag = $(this.searchTag).val();
+    if(searchTag != ""){
+      search = $.ajax("http://api.giphy.com/v1/gifs/search?q=" + searchTag + "&api_key=dc6zaTOxFJmzC");
+    } else{
+      alert("You must provide a search term!");
+    }
+    search.then(function(data){
+      j = 0;
+      i = 0;
+      $(".container").empty();
+      for(i = 0; i< 5; i++){
+        thing = data.data[i].images.original.url;
+        $(".container").append('<img src="' + thing + '" />');
+      }
+      $(".next5").on("click", function(){
+        $(".container").empty();
+        var j = i + 5;
+        for(i; i< j; i++){
+          if(i === data.data.length){
+            $(".container").append('<p>All out of wacky pictures! Try a new search!</p>');
+            return;
+          }else{
+            thing = data.data[i].images.original.url;
+            $(".container").append('<img src="' + thing + '" />');
+          }
+        }
+      });
+    }).catch(function(){
+      $(".container").append("<p>nothing appeared</p>")
+    });
+  });
 
 
 
